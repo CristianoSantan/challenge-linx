1462bc30e0280833b71803ba0a18d66d6c257e5f
CristianoSantan <cristiano.mer@gmail.com>
2021-04-12 04:10:52 -0300

    validation form share

--- JS/scripts.js
+++ JS/scripts.js
@@ -45,3 +45,82 @@ const main = async () => {
 
 main();
 
+// -------------------------------------------------------- Validation of the sharing form
+
+const fields = document.querySelectorAll("[required]")
+
+function validateField(field) {
+  // logica para verificar se existem erros
+  function verifyErrors() {
+    let foundError = false;
+
+    for(let error in field.validity) {
+      // se não for customError
+      // então verifica se tem erro
+      if (field.validity[error] && !field.validity.valid) {
+        foundError = error
+      }
+    }
+    return foundError;
+  }
+  // Lógica para customizar a mensagem
+  function customMessage(typeError) {
+    const messages = {
+      text: {
+        valueMissing: "* Por favor, preencha este campo"
+      },
+      email: {
+        valueMissing: "* Email é obrigatório",
+        typeMismatch: "* Por favor, preencha um email válido"
+      }
+    }
+    return messages[field.type][typeError]
+  }
+  // Lógica para atualizar a mensagem
+  function setCustomMessage(message) {
+    const spanError = field.parentNode.querySelector("span.error")
+    if (message) {
+      spanError.classList.add("active")
+      spanError.innerHTML = message
+    } else {
+      spanError.classList.remove("active")
+      spanError.innerHTML = ""
+    }
+  }
+  // Retorna uma função com uma lógica de condição
+  return function () {
+    const error = verifyErrors()
+    if (error) {
+      const message = customMessage(error)
+      field.style.backgroundColor = "#ff000005"
+      field.style.borderColor = "#ff000080"
+      setCustomMessage(message)
+    } else {
+      field.style.backgroundColor = "#00ff0002"
+      field.style.borderColor = "#00ff0090"
+      setCustomMessage()
+    }
+  }
+}
+// recebe o evento de cada campo e retorna a mensagem
+function customValidation(event) {
+  const field = event.target
+  const validation = validateField(field)
+  validation()
+}
+// qual campo disparou o evento
+for ( field of fields ) {
+  field.addEventListener("invalid", event => {
+    // eliminar o bubble do navegador
+    event.preventDefault()
+    customValidation(event)
+  })
+  field.addEventListener("blur", customValidation)
+}
+// Não enviar o formulario ao submeter 
+document.querySelector("#form-share")
+.addEventListener("submit", event => {
+  console.log("enviar o formulário")
+  // não envia o formuário
+  event.preventDefault()
+})
